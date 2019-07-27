fb5765322d00da7df6eb87e8f13a571b1e17e7aa
vagrant <vagrant@vagrant.vm>
2019-07-27 03:42:54 +0000

    add

--- aptGetUpdate.sh
+++ aptGetUpdate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-apt-get update
-apt-get -y install git
-apt-get -y install fish
-apt-get -y install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
-#apt-get -y install openssl
-#apt-get -y install python3-pip
-apt-get -y install xserver-xorg
+sudo apt-get update
+sudo apt-get -y install git
+sudo apt-get -y install fish
+sudo apt-get -y install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+sudo apt-get -y install openssl
+sudo apt-get -y install python3-pip
+sudo apt-get -y install xserver-xorg
--- initNvim.sh
+++ initNvim.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+
+cd `dirname $0`
+nvimPath=~/neovim
 if type nvim; then
 	echo "nvim"
 else
-	git clone https://github.com/neovim/neovim.git
-	cd ~/neovim
+	git clone https://github.com/neovim/neovim.git $nvimPath
+	cd $nvimPath
+	pwd
 	make CMAKE_BUILD_TYPE=RelWithDebInfo
 	sudo make install
-	cd
 fi
 
+cd `dirname $0`
 deinCachePath=~/.cache/dein
-installerPath=~/deinInstaller.sh
 if [ -e $deinCachePath ]; then
 	echo $deinCachePath
 else
+	installerPath=~/deinInstaller.sh
 	curl -sS https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > $installerPath
 	sh $installerPath $deinCachePath
 	rm $installerPath
 fi
 
+cd `dirname $0`
 python3 -m pip install --user --upgrade pynvim
